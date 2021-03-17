57498a0b5a83d38c62673ac5229703bd65713583
unknown <aelise17264@gmail.com>
2021-03-17 17:06:22 -0600

    keydown commands added

--- index.js
+++ index.js
@@ -53,3 +53,41 @@ for (i = 0; i < drums; i++){
        
     })
 }
+
+document.addEventListener("keydown", function(event){
+    console.log(event)
+    var thisKey = event.key
+    switch (thisKey){
+        case "w":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+        break;
+        case "a":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+        break;
+        case "s":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+        break;
+        case "d":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+        break;
+        case "j":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+        break;
+        case "k":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+        break;
+        case "l":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+        break;
+            
+    default: console.log(buttonHTML)
+
+    }
+})
